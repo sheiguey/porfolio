c7e3eeb286735bc7552e49329bce16b384eac06f
sheiguey <40198193+sheiguey@users.noreply.github.com>
2023-06-08 22:23:01 +0100

    update resume

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ export default function Header(){
                     <p>Salut moi c'est</p>
                     <h1 className={classes.title}> Franky Shiti</h1>
                     <h2>Developpeur Frontend </h2>
-                    <a alt="mon cv" href="/assets/cv.pdf" target="_blank" className={classes.button}>Mon CV</a>
+                    <a alt="mon cv" href="/assets/resume-FrankyShiti.pdf" target="_blank" className={classes.button}>Mon CV</a>
                 </div>
                 <div  className={`${classes.profil}  fadeInUp  animate__animated `}></div>
             </div>
